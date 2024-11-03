358ae7baa01ee17d7013d1130a1ae63b595165f3
sermuns <sermuns@lysator.liu.se>
2024-11-03 15:54:54 +0100

    more uppgifter

--- book/src/ellara/uppgifter/index.md
+++ book/src/ellara/uppgifter/index.md
@@ -79,17 +79,51 @@ $$ U = 1\kilo\ohm \cdot 23\milli\ampere = 23\volt $$
 
 </li>
 
-
 <li>
 
-Hur mycket spänning ligger över varje resistor?
+a) Hur stor är den **totala spänningen**?
+
+b) Hur stor är **strömmen** i kretsen?
 
 ![](ks4.svg)
 
 <details>
 <summary>Svar</summary>
 
-**SVAR:** $U_1 = 4\volt$, $U_2 = 8\volt$
+**SVAR:**
+
+a) $U = 6\volt$
+
+b) $I = 6\milli\ampere$
+
+
+---
+
+$U_1 = 3\volt$, $U_2 = 3\volt$, $R = 1\kilo\ohm$.
+
+a) $U = U_1 + U_2 = 3\volt + 3\volt = 6\volt$
+
+b) $I = \frac{U}{R} = \frac{6\volt}{1\kilo\ohm} = 6\milli\ampere$
+
+</details>
+
+</li>
+
+<li>
+
+a) Hur stor är del**spänningen** över varje resistor?
+
+b) Hur stor är **strömmen** i kretsen?
+
+![](ks5.svg)
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+a) $U_1 = 4\volt$, $U_2 = 8\volt$
+
+b) $I = 2\milli\ampere$
 
 ---
 
@@ -121,41 +155,176 @@ $$ U_2 = U - U_1 = 12\volt - 4\volt = 8\volt $$
 
 <li>
 
-Hur mycket spänning ligger över varje resistor?
+Vad behöver den högra kretsen ha för **resistans** för att få samma ström som den vänstra?
 
-![](ks4.svg)
+![](ks6.svg)
 
 <details>
 <summary>Svar</summary>
 
-**SVAR:** $U_1 = 4\volt$, $U_2 = 8\volt$
+**SVAR:** $R _ {ers} \approx 33.3\ohm$
 
 ---
 
-Man kan lösa detta på två sätt: antingen genom att först räkna ut totala strömmen och applicera Ohms lag, eller så kan man ta hjälp av **KVL**.
+Frågan är egentligen, vad är ersättningresistansen för den vänstra kretsen? Eftersom strömmen också är given skulle man kunna lösa problemet utan att beräkna ersättningsresistansen, dock. Här är två sätt att lösa det:
+
+**Ersättningsresistans:**
+
+$R_1=100\ohm$, $R_2=50\ohm$.
+
+$$ R _ {ers} = \frac{1}{\frac{1}{R_1} + \frac{1}{R_2}} = \frac{1}{\frac{1}{100\ohm} + \frac{1}{50\ohm}} \approx 33.3\ohm $$
 
 **Ohms lag:**
 
-$U = 12\volt$, $R_1=2\kilo\ohm$, $R_2=4\kilo\ohm$.
+$U = 5\volt$, $I = 150\milli\ampere$.
 
-$R _ {tot} = R_1 + R_2 = 6\kilo\ohm$
-
-$$ I = \frac{U}{ R _ {tot} } = \frac{12\volt}{6\kilo\ohm} = 2\milli\ampere $$
-
-$$ U_1 = I R_1 = 2\milli\ampere \cdot 2\kilo\ohm = 4\volt $$
-
-$$ U_2 = I R_2 = 2\milli\ampere \cdot 4\kilo\ohm = 8\volt $$
-
-**KVL:**
-
-$R _ {tot} = R_1 + R_2 = 6\kilo\ohm$
-
-$$ U_1 = U * \frac{R_1}{R _ {tot}} = 12\volt \cdot \frac{2\kilo\ohm}{6\kilo\ohm} = 4\volt $$
-
-$$ U_2 = U - U_1 = 12\volt - 4\volt = 8\volt $$
+$$ R _ {ers} = \frac{U}{I} = \frac{5\volt}{150\milli\ampere} \approx 33.3\ohm $$
 
 </details>
 
 </li>
 
+
+
+
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
+<li>
+
+![]()
+
+<details>
+<summary>Svar</summary>
+
+**SVAR:** 
+
+---
+
+</details>
+
+</li>
 </ol>
--- book/src/ellara/uppgifter/svg.py
+++ book/src/ellara/uppgifter/svg.py
@@ -1,44 +1,82 @@
 import schemdraw
-import schemdraw.elements as elm
+import schemdraw.elements as e
 import schemdraw.elements
 
-elm.style(elm.STYLE_IEC)
-schemdraw.config(font="NewComputerModernMath",mathfont="cm")
+e.style(e.STYLE_IEC)
+schemdraw.config(font="NewComputerModernMath", mathfont="cm")
+
+counter = 0
+
 
 built = 0
 
-with schemdraw.Drawing(file="ks1.svg", show=False) as d:
-    elm.BatteryCell().left().label('10 V')
-    up_line = elm.Line().up()
-    current_up = elm.CurrentLabelInline(ofst=0, direction='out', color='red').at(up_line)
-    elm.Resistor().right().label('2 kΩ')
-    down_line = elm.Line().down()
-    current_down = elm.CurrentLabelInline(ofst=0, direction='out', color='red').at(down_line).label('I')
+with schemdraw.Drawing(file=f"ks1.svg", show=False) as d:
+    e.BatteryCell().left().label('10 V')
+    up_line = e.Line().up()
+    current_up = e.CurrentLabelInline(
+        ofst=0, direction='out', color='red').at(up_line)
+    e.Resistor().right().label('2 kΩ')
+    down_line = e.Line().down()
+    current_down = e.CurrentLabelInline(
+        ofst=0, direction='out', color='red').at(down_line).label('I')
     built += 1
 
-with schemdraw.Drawing(file="ks2.svg", show=False) as d:
-    elm.BatteryCell().left().label('3.3 V')
-    up_line = elm.Line().up()
-    elm.Resistor().right()
-    down_line = elm.Line().down()
-    current_down = elm.CurrentLabelInline(ofst=0, direction='out').at(down_line).label('6.6 mA')
+with schemdraw.Drawing(file=f"ks2.svg", show=False) as d:
+    e.BatteryCell().left().label('3.3 V')
+    up_line = e.Line().up()
+    e.Resistor().right()
+    down_line = e.Line().down()
+    current_down = e.CurrentLabelInline(
+        ofst=0, direction='out').at(down_line).label('6.6 mA')
     built += 1
 
-with schemdraw.Drawing(file="ks3.svg", show=False) as d:
-    elm.BatteryCell().left()
-    up_line = elm.Line().up()
-    elm.Resistor().right().label('1 kΩ')
-    down_line = elm.Line().down()
-    current_down = elm.CurrentLabelInline(ofst=0, direction='out').at(down_line).label('23 mA')
+with schemdraw.Drawing(file=f"ks3.svg", show=False) as d:
+    e.BatteryCell().left()
+    up_line = e.Line().up()
+    e.Resistor().right().label('1 kΩ')
+    down_line = e.Line().down()
+    current_down = e.CurrentLabelInline(
+        ofst=0, direction='out').at(down_line).label('23 mA')
     built += 1
 
-with schemdraw.Drawing(file="ks4.svg", show=False) as d:
-    elm.BatteryCell().left(6).label('12 V')
-    up_line = elm.Line().up()
-    elm.Resistor().right().label('$R_1$ = 2 kΩ')
-    elm.Resistor().right().label('$R_2$ = 4 kΩ')
-    down_line = elm.Line().down()
+with schemdraw.Drawing(file=f"ks4.svg", show=False) as d:
+    e.BatteryCell().right().label('3 V')
+    e.BatteryCell().right().label('3 V')
+    e.Line().down()
+    e.Resistor().left(2*d.unit).label('1 kΩ')
+    e.Line().up()
+    
     built += 1
+
+with schemdraw.Drawing(file=f"ks5.svg", show=False) as d:
+    e.BatteryCell().left(6).label('12 V')
+    up_line = e.Line().up()
+    e.Resistor().right().label('$R_1$ = 2 kΩ')
+    e.Resistor().right().label('$R_2$ = 4 kΩ')
+    down_line = e.Line().down()
+    built += 1
+
+with schemdraw.Drawing(file=f"ks6.svg", show=False) as d:
+    u = d.unit
+    e.BatteryCell().left().label('5 V')
+    e.Line().up(.6*u)
+    d.push()
+    e.Resistor().right().label('50 Ω')
+    d.pop()
+    e.Line().up(.6*u)
+    e.Resistor().right().label('100 Ω')
+    down_line = e.Line().down(1.2*u)
+    e.CurrentLabelInline(
+        ofst=-.4*u, direction='out').at(down_line).label('150 mA', loc='B')
+
+    d.move(2*d.unit, 0)
+
+    e.BatteryCell().left().label('5 V')
+    e.Line().up(.6*u)
+    e.Resistor().right()
+    e.Line().down(.6*u)
+
+    built += 1
+
 
 print(f"Built {built} files")
-
