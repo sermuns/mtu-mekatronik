2fc06d818bd112b93e25f77db93a4453528a7905
sermuns <sermuns@lysator.liu.se>
2024-11-16 18:52:20 +0100

    finally working

--- arduino/AnalogToDigital/AnalogToDigital.ino
+++ arduino/AnalogToDigital/AnalogToDigital.ino
@@ -1,14 +1,19 @@
 #define INPUT_PIN A0
-#define OUTPUT_PIN 13
+
+char buffer[50];
+unsigned int analogValue;
 
 void setup() {
-//	Serial.begin(9600);
+	Serial.begin(115200);
 	pinMode(INPUT_PIN, INPUT);
-	pinMode(OUTPUT_PIN, OUTPUT);
 }
 
 void loop() {
-	const int sensorValue = analogRead(INPUT_PIN);
-//	Serial.println(sensorValue);
-	digitalWrite(OUTPUT_PIN, sensorValue > 512 ? HIGH : LOW);
+  delay(50);
+  analogValue = analogRead(INPUT_PIN);
+  float millivolts = analogValue * (5000.0 / 1023.0);
+
+  Serial.print('$');
+  Serial.print(millivolts);
+  Serial.println(';');
 }
